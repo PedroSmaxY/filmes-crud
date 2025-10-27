c7bf66dba7b074a772ff6e1cecab3e17cec3e168
PedroSmaxY <pedrohnovais@outlook.com>
2025-10-27 18:31:54 -0300

    Corrigindo nome de entidade

--- src/main/java/com/facul/filmes/repositories/RentailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.facul.filmes.repositories;
-
-import com.facul.filmes.domain.entities.Rentail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentailRepository extends JpaRepository<Rentail, Integer> {
-}
