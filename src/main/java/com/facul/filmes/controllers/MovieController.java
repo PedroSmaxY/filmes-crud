106e4abb1f09bf84a665cfe446866948e578d27e
PedroSmaxY <pedrohnovais@outlook.com>
2025-10-27 17:18:30 -0300

    Adicionando robustez aos endpoints existentes

--- src/main/java/com/facul/filmes/controllers/MovieController.java
+++ src/main/java/com/facul/filmes/controllers/MovieController.java
@@ -2,8 +2,11 @@ package com.facul.filmes.controllers;
 
 import com.facul.filmes.domain.entities.Movie;
 import com.facul.filmes.services.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,14 +23,27 @@ public class MovieController {
         return service.listAllMovies();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findMovieById(id));
+    }
+
     @PostMapping
-    public Movie add(@RequestBody Movie movie) {
-        return service.add(movie);
+    public ResponseEntity<Movie> add(@RequestBody Movie movie) {
+        Movie newMovie = this.service.add(movie);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newMovie.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newMovie);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        service.remove(id);
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Movie movie = this.service.findMovieById(id);
+        this.service.remove(movie.getId());
+
+        return ResponseEntity.noContent().build();
     }
 
 }
