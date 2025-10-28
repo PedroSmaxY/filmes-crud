d099255da5a5157fc32737c9f1a2b52ba607549f
PedroSmaxY <pedrohnovais@outlook.com>
2025-10-28 13:15:17 -0300

    Completando CRUD de customers

--- src/main/java/com/facul/filmes/services/CustomerService.java
+++ src/main/java/com/facul/filmes/services/CustomerService.java
@@ -2,8 +2,12 @@ package com.facul.filmes.services;
 
 import com.facul.filmes.domain.entities.Customer;
 import com.facul.filmes.repositories.CustomerRepository;
+import com.facul.filmes.services.exceptions.DuplicateResourceException;
 import com.facul.filmes.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
@@ -13,13 +17,59 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public Customer findById(Integer id) {
-        return customerRepository.findById(id.longValue())
+    public List<Customer> findAll() {
+        return this.customerRepository.findAll();
+    }
+
+    public Customer findById(Long id) {
+        return customerRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Customer not found with id " + id));
     }
 
+    public Customer findByEmail(String email) {
+        return customerRepository.findByEmailIgnoreCase(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with email " + email));
+    }
+
+    public Customer findByPhone(String phone) {
+        return customerRepository.findByPhone(phone)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with phone " + phone));
+    }
+
+    @Transactional
     public Customer save(Customer customer) {
+        this.customerRepository
+                .findByPhoneOrEmailIgnoreCase(customer.getPhone(), customer.getEmail())
+                .ifPresent(c -> { throw new DuplicateResourceException("Customer with the same phone or email already exists."); });
 
         return customerRepository.save(customer);
+    }
+
+    @Transactional
+    public void update(Long id, Customer customer) {
+        Customer existingCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with id " + id));
+
+        this.customerRepository
+                .findByPhoneOrEmailIgnoreCase(customer.getPhone(), customer.getEmail())
+                .ifPresent(c -> {
+                    if (!c.getId().equals(id)) {
+                        throw new DuplicateResourceException("Another customer with the same phone or email already exists.");
+                    }
+                });
+
+        existingCustomer.setName(customer.getName());
+        existingCustomer.setEmail(customer.getEmail());
+        existingCustomer.setPhone(customer.getPhone());
+
+        customerRepository.save(existingCustomer);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        if (!customerRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Customer not found with id " + id);
+        }
+        customerRepository.deleteById(id);
     }
 }
