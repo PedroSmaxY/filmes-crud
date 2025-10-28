3cf28106f73c4800994d7c1fd3f46bbcc954d943
PedroSmaxY <pedrohnovais@outlook.com>
2025-10-28 12:07:54 -0300

    Corrigindo tipagem do id

--- src/main/java/com/facul/filmes/repositories/CustomerRepository.java
+++ src/main/java/com/facul/filmes/repositories/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.facul.filmes.repositories;
 import com.facul.filmes.domain.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
