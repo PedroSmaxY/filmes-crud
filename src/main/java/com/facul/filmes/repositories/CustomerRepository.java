4d0602d6519873d83ebedfad614672fe7a9cdd52
PedroSmaxY <pedrohnovais@outlook.com>
2025-10-28 13:14:42 -0300

    Adicionando querys de phone e email

--- src/main/java/com/facul/filmes/repositories/CustomerRepository.java
+++ src/main/java/com/facul/filmes/repositories/CustomerRepository.java
@@ -3,5 +3,10 @@ package com.facul.filmes.repositories;
 import com.facul.filmes.domain.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByPhoneOrEmailIgnoreCase(String phone, String email);
+    Optional<Customer> findByEmailIgnoreCase(String email);
+    Optional<Customer> findByPhone(String phone);
 }
