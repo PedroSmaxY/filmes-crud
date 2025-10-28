3e3a36a1323b706c5ebca1d632601bb8b2316734
PedroSmaxY <pedrohnovais@outlook.com>
2025-10-28 16:29:01 -0300

    Adicionando Dto para facilitar o output

--- src/main/java/com/facul/filmes/controllers/RentalController.java
+++ src/main/java/com/facul/filmes/controllers/RentalController.java
@@ -2,12 +2,14 @@ package com.facul.filmes.controllers;
 
 import com.facul.filmes.domain.entities.Rental;
 import com.facul.filmes.dto.rental.RentalRequestDTO;
+import com.facul.filmes.dto.rental.RentalResponseDTO;
 import com.facul.filmes.services.RentalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rentals")
@@ -18,8 +20,25 @@ public class RentalController {
         this.rentalService = rentalService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<RentalResponseDTO>> getAllRentals() {
+        var rentals = this.rentalService.findAll()
+                .stream()
+                .map(r -> new RentalResponseDTO(
+                        r.getId(),
+                        r.getMovie().getId(),
+                        r.getCustomer().getId(),
+                        r.getProcessedBy().getId(),
+                        r.getRentalDate(),
+                        r.getReturnDate(),
+                        r.getStatus()
+                )).toList();
+
+        return ResponseEntity.ok(rentals);
+    }
+
     @PostMapping
-    public ResponseEntity<Rental> createRental(@RequestBody RentalRequestDTO dto) {
+    public ResponseEntity<RentalResponseDTO> createRental(@RequestBody RentalRequestDTO dto) {
         var rental = this.rentalService.createRental(dto.movieId(), dto.customerId(), dto.employeeId());
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -27,7 +46,17 @@ public class RentalController {
                 .buildAndExpand(rental.getId())
                 .toUri();
 
-        return ResponseEntity.created(uri).body(rental);
+        RentalResponseDTO rentalResponseDTO = new RentalResponseDTO(
+                rental.getId(),
+                rental.getMovie().getId(),
+                rental.getCustomer().getId(),
+                rental.getProcessedBy().getId(),
+                rental.getRentalDate(),
+                rental.getReturnDate(),
+                rental.getStatus()
+        );
+
+        return ResponseEntity.created(uri).body(rentalResponseDTO);
     }
 
     @PatchMapping("/{rentalId}/return")
