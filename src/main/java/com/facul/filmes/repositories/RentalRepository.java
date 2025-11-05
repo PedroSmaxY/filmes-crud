e30ab662733e5b3a7d030792d1192dd5ad6e3cfc
PedroSmaxY <pedrohnovais@outlook.com>
2025-11-05 12:27:01 -0300

    Adicionando método existsByCustomerAndMovieIdAndStatus no RentalRepository

--- src/main/java/com/facul/filmes/repositories/RentalRepository.java
+++ src/main/java/com/facul/filmes/repositories/RentalRepository.java
@@ -1,7 +1,9 @@
 package com.facul.filmes.repositories;
 
 import com.facul.filmes.domain.entities.Rental;
+import com.facul.filmes.domain.enums.RentalStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    Boolean existsByCustomerAndMovieIdAndStatus(Long customerId, Long movieId, RentalStatus status);
 }
